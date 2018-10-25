e85aea942d656176f56a6ac5a14e64d84e282672
Murtaz Raina <42265864+murtaz25raina@users.noreply.github.com>
2018-10-25 18:49:12 +0530

    Update QueUeWith2Stacks.java

--- QueUeWith2Stacks.java
+++ QueUeWith2Stacks.java
@@ -1,128 +1,48 @@
-class Que1
-{
-	private int data;
-	private Que1 next;
-	Que1 head =null;
-	public void push(int data2)
-	{
-		Que1 temp = new Que1();
-		temp.data=data2;
-		if(head==null)
-		{
-			head=temp;
-		}
-		else
-		{
-			Que1 ptr = head;
-			while( ptr.next != null )
-			{
-				ptr = ptr.next;
-			}
-			ptr.next = temp;
-		}
-	}
-	public int pop()
-	{
-		Que1 temp = null;
-		if(head==null)
-			System.out.println("underflow");
-		else 
-		{
-			temp=head;
-		    head=head.next;
-		}
-		return temp.data;
-	}
-	public boolean isEmpty()
-	{
-		if(head==null)
-			return true;
-		return false;
-	}
-	public void display()
-	{
-		Que1 ptr = head;
-		while(ptr!=null)
-		{
-			System.out.print(ptr.data+" ");
-			ptr =ptr.next;
-		}
-		System.out.println();
-	}
-}
-
-class Stck1
-{
-	private int data;
-	private Stck1 next;
-	Stck1 Head1=null;
-	public void push(int data2)
-	{
-		Stck1 temp = new Stck1();
-		temp.data=data2;
-		if(Head1==null)
-			Head1=temp;
-		else
-		{
-			temp.next=Head1;
-			Head1=temp;
-		}
-	}
-	public int pop()
-	{
-	    Stck1 temp = null;
-		if(Head1==null)
-			System.out.println("underflow");
-		else 
-		{
-			temp=Head1;
-		    Head1=Head1.next;
-		}
-		return temp.data;
-	}
-	public boolean isEmpty()
-	{
-		if(Head1==null)
-			return true;
-		return false;
-	}
-	public void display()
-	{
-		Stck1 ptr = Head1;
-		while(ptr!=null)
-		{
-			System.out.print(ptr.data+" ");
-			ptr=ptr.next;
-		}
-		System.out.println();
-	}
-}
-
-public class QueUeWith2Stacks {
-
-	public static void main(String[] args) {
-		Que1 Q1 = new Que1();
-		System.out.println("creating Queue...........");
-		for(int i=1;i<=5;i++)
-		 Q1.push(i*10);
-		System.out.println("displaying Queue...........");
-		Q1.display();
-		Stck1 S1 = new Stck1();
-		Stck1 S2 = new Stck1();
-		System.out.println("transfering Queue to first Stack.............");
-		while(true)
-		{
-			if(Q1.isEmpty())
-				break;
-			S1.push(Q1.pop());
-		}
-		System.out.println("transfering first Stack into second Stack............... ");
-		while(true)
-		{
-			if(S1.isEmpty())
-				break;
-			S2.push(S1.pop());
-		}
-		S2.display();
-	}
+import java.util.*; 
+  
+class HelloWorld  
+{  
+static class Queue  
+{  
+    static Stack<Integer> s1 = new Stack<Integer>();  
+    static Stack<Integer> s2 = new Stack<Integer>();  
+  
+    static void enQueue(int x)  
+    {  
+        while (!s1.isEmpty()) 
+        {  
+            s2.push(s1.pop());  
+        }  
+  
+        s1.push(x);  
+        while (!s2.isEmpty())  
+        {  
+            s1.push(s2.pop());  
+        }  
+    }  
+  
+    static int deQueue()  
+    {  
+        if (s1.isEmpty())  
+        {  
+            System.out.println("Q is Empty");  
+            System.exit(0);  
+        }  
+        int x = s1.peek();  
+        s1.pop();  
+        return x;  
+    }  
+};  
+  
+public static void main(String[] args)  
+{  
+    Queue q = new Queue();  
+    q.enQueue(1);  
+    q.enQueue(2);  
+    q.enQueue(3);  
+  
+    System.out.println(q.deQueue());  
+    System.out.println(q.deQueue()); 
+    System.out.println(q.deQueue()); 
+}  
 }
